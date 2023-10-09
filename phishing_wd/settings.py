89280056a0191e8a56f2f317b231dd1a1df7b4e4
Asif <muhammedasifs321@gmail.com>
2023-10-09 22:36:16 +0530

    Add static files

--- phishing_wd/settings.py
+++ phishing_wd/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# Add STATIC_ROOT to specify where collected static files will be stored
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
